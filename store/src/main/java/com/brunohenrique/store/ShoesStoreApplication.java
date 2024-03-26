630446e32225aea9126d15f55dc4c53f50c5771a
Bruno <brunohenrrique.0303@gmail.com>
2024-03-25 21:47:13 -0300

    Configurações iniciais

---
+++ store/src/main/java/com/brunohenrique/store/ShoesStoreApplication.java
@@ -0,0 +1,13 @@
+package com.brunohenrique.store;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShoesStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShoesStoreApplication.class, args);
+	}
+
+}
